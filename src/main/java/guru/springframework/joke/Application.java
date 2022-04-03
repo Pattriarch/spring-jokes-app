d6a8cb0d694ef4c19ac45e89381c5ed6020d17ed
Pattriarch <pgelo298@mail.ru>
2022-04-03 22:38:42 +0300

    Initail commit

--- src/main/java/guru/springframework/joke/Application.java
+++ src/main/java/guru/springframework/joke/Application.java
@@ -2,12 +2,13 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
